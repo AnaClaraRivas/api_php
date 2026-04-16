8fedbba6c37696d49cee161d8e479b527babb35c
Ana Clara Bronzeri Rivas <159500946+AnaClaraRivas@users.noreply.github.com>
2026-04-16 10:16:48 -0300

    16/04

--- AULA06/api.php
+++ AULA06/api.php
@@ -7,26 +7,58 @@
 
     // echo "Método da requisição: ".$metodo;
 
+    // Recupera o arquivo json na mesma pasta do proejto
+    $arquivo = 'usuarios.json';
+
+    // Verifica se o arquivo existe, se nao cria um array vazio
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    // Le o conteudo do arquivo json
+    $usuarios = json_decode(file_get_contents($arquivo),true);
+
+    // Conteudo
+    // $usuarios = [
+    //      [ "id" => 1, "nome" => "Ana Clara", "email" => "ana@email.com"],
+    //      [ "id" => 2, "nome" => "Byanca", "email" => "byanca@email.com"],
+    // ];
+
     switch  ($metodo) {
+
         case 'GET':
-            echo "AQUI AÇÕES EM MÉTODO GET";
+            // echo "AQUI AÇÕES EM MÉTODO GET";
+
+            echo json_encode($usuarios);
+
             break;
+
         case 'POST':
-            echo "AQUI AÇÕES DO MÉTODO POST";
+            // echo "AQUI AÇÕES DO MÉTODO POST";
+
+            $dados = json_decode(file_get_contents('php://input'),true);
+            // print_r($dados);
+
+            $novoUsuario = [
+                "id" => $dados["id"], 
+                "nome" => $dados["nome"], 
+                "email" => $dados["email"]
+            ];
+
+            // adiciona o novo usuario ao array existente
+            array_push($usuarios, $novoUsuario);
+            echo json_encode('Usuario inserido com sucesso!!');
+            print_r($usuarios);
+
             break;
+            
         default:
             echo "MÉTODO NÃO ENCONTRADO!";
             break;
     }
 
 
-    // // Conteudo
-    // $usuarios = [
-    //     [ "id" => 1, "nome" => "Ana Clara", "email" => "ana@email.com"],
-    //     [ "id" => 2, "nome" => "Byanca", "email" => "byanca@email.com"],
-    // ];
-
-    // // Converte ára JSON e retorna
+    // // Converte para JSON e retorna
     // echo json_encode($usuarios);
 
 ?>
